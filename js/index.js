42b6c8de8ef805527a95c75288e5ab76ff5ac55d
wjsyejin <hanau1379@gmail.com>
2023-03-20 15:07:44 +0900

    230320 공통모듈 분리

--- js/index.js
+++ js/index.js
@@ -16,29 +16,6 @@ window.onload = function(){
 }
 
 
-// search_bar의 내용들을 가져와서 searchEl에 저장할 것이다.
-const searchEl = document.querySelector('.search_bar');
-const searchInputEl = searchEl.querySelector('input');
-console.log(searchEl);
-console.log(searchInputEl);
-
-// searchEl 안의 로고를 클랙했을 때 searchEl.inputEl(입력칸)를 focus 상태(입력가능상태)로 바꿔주도록 리스너 기능을 추가
-searchEl.addEventListener('click', function(){
-    searchInputEl.focus();
-});
-
-// focused 시에는 이런 내용이 나타날 것이다.
-searchInputEl.addEventListener('focus', function(){
-    searchEl.classList.add('focused');
-    searchInputEl.setAttribute('placeholder', '검색어를 입력해주세요');
-});
-
-
-// focused가 되면 이런 상태가 된다.
-searchInputEl.addEventListener('blur', function(){
-    searchEl.classList.remove('focused');
-    searchInputEl.setAttribute('placeholder', '');
-});
 
 // badges 고정하기
 // 좀 더 정확하게 쓰고 싶으면 상세 경로를 써줘라
